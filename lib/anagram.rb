3a186cade65eb4ea08281840bb12d02f203524a9
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-20 20:12:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,5 @@
 # Your code goes here!
 class Anagram
-
-#match method takes an array of possible anagrams. It should return all matches in an array. If no matches exist, it should return an empty array.  
   
   attr_accessor :word
   
@@ -18,23 +16,8 @@ class Anagram
       end
       matches
     end 
-
-  end#How will you determine if one word is an anagram for another
-  
-  #break each word down into letter elements and then sort them and compare for a match
-  
-    def match(array)
-    matches = []
-
-    array.each do |e|
-      if e.split("").sort ==  @word.split("").sort
-        matches << e
-      end
-      matches
-    end 
-
   end
   
-end
+end #Anagram
 
 
